61b8dc2eb0fd97cb0363f8192f95eb70367e6353
profesor-julioeld <profesor.julioeld@iesazarquiel.es>
2023-02-18 18:22:00 +0100

    Commit inicial

---
+++ src/test/java/com/s2daw/reactspringbackend/ReactSpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s2daw.reactspringbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
