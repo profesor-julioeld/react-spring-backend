1963bc3ff92c57fe558d8330ddc4995b0a5fc223
profesor-julioeld <profesor.julioeld@iesazarquiel.es>
2023-02-19 12:15:53 +0100

    Añadimos recurso /api/hora devuelve h:mm:ss

---
+++ src/main/java/com/s2daw/reactspringbackend/controllers/HolaController.java
@@ -0,0 +1,22 @@
+package com.s2daw.reactspringbackend.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HolaController {
+    @RequestMapping(value="api/hora")
+    public Map getHora(){
+        Map respuesta=new HashMap();
+        // Muestro sólo hora y minuto, no muestro segundos
+        LocalTime hora = LocalTime.now();
+        DateTimeFormatter f = DateTimeFormatter.ofPattern("h:mm:ss");
+        respuesta.put("Hora", hora.format(f));
+        return respuesta;
+    }
+}
